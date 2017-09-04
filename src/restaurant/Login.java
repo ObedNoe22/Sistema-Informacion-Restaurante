0bab8483b325bf8f08c255f4fe618fae72fce9a1
ObedNoe22 <obednoe22@gmail.com>
2017-09-03 20:57:14 -0500

    Reservaciones abre y cierra

--- src/restaurant/Login.java
+++ src/restaurant/Login.java
@@ -39,6 +39,11 @@ public class Login extends javax.swing.JFrame {
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/usuarios.png"))); // NOI18N
 
         jButton1.setText("Ingresar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setText("Contraseña");
 
@@ -89,6 +94,14 @@ public class Login extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        Reserv reserva=new Reserv();
+        reserva.setVisible(true);
+        this.setVisible(false);
+                
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
